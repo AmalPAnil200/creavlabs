57429b60782bf2d655159395eb5753bfae20310e
AmalPAnil200 <amalpanilebsgl@gmail.com>
2026-03-13 19:10:27 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/creavlabs/tree/main/creavlabs",
+  base: process.env.VITE_BASE_PATH || "/creavlabs/tree/main/frontend",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
